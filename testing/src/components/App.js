ac3c1cd3fad9c78af45586f68660c02f9610c4ba
Devesh Avutu <deveshavutu@Deveshs-MacBook-Air.local>
2019-09-15 10:37:04 +0530

    created testing project and created a higher order function

---
+++ testing/src/components/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Route, Link} from 'react-router-dom';
+import CommentBox from './CommentBox';
+import CommentList from './CommentList';
+import {connect} from 'react-redux';
+import {changeAuth} from '../actions';
+
+class App extends React.Component{
+  renderButton() {
+    if(this.props.auth){
+      return(
+        <button onClick={() => this.props.changeAuth(false)}>SignOut</button>
+      );
+    } else {
+      return(
+        <button onClick={() => this.props.changeAuth(true)}>SignIn</button>
+      )
+    }
+  }
+  renderHeader() {
+    return(
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/post">Post a comment</Link></li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+
+  }
+
+  render(){
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path='/post' component={CommentBox} />
+        <Route path='/' exact component={CommentList}/>
+      </div>
+    );
+  }
+
+};
+
+const mapStateToProps = (state) => {
+  return {auth: state.auth};
+}
+
+export default connect(mapStateToProps, {changeAuth})(App);
---
+++ testing/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import Root from './Root';
+import App from './components/App';
+
+ReactDOM.render(
+  <Root>
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  </Root>,
+  document.querySelector('#root')
+);
